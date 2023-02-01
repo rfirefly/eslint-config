dcd5fe52a9c7c75e00dda6bbc67758de98d28848
hf <hufan@lingxing.com>
2023-02-01 15:43:31 +0800

    chore: change vue component-tags-order

--- packages/vue/index.js
+++ packages/vue/index.js
@@ -40,7 +40,7 @@ module.exports = {
     'vue/no-setup-props-destructure': 'off',
 
     'vue/component-tags-order': ['error', {
-      order: ['script', 'template', 'style'],
+      order: ['template', 'script', 'style'],
     }],
     'vue/block-tag-newline': ['error', {
       singleline: 'always',
